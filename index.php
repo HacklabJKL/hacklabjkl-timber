59de5394c4c25dca94807ed4185fc661f05a1866
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-05-25 17:12:55 +0300

    Posts and news queries

--- index.php
+++ index.php
@@ -14,10 +14,29 @@
  */
 
 $context          = Timber::context();
-$context['posts'] = Timber::get_posts();
-$context['foo']   = 'bar';
+
+$news_query_args = [
+	'post_type'	=> 'post',
+	'category_name' => 'projekti,kerhoilta,tapahtuma,yhdistys',
+];
+$context['news'] = Timber::get_posts($news_query_args);
+
+$posts_query_args = [
+	'post_type'	=> 'post',
+	'category_name' => 'projekti,kerhoilta,tapahtuma,yhdistys',
+];
+$context['posts'] = Timber::get_posts($posts_query_args);
+
+
 $templates        = array( 'index.twig' );
+
+// WP template hierarchy boilerplate code
 if ( is_home() ) {
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
 }
+
+// For HacklabJKL site index.twig template is simply used, for more info read:
+// https://timber.github.io/docs/v2/getting-started/template-inheritance-and-includes/
+// https://developer.wordpress.org/themes/functionality/custom-front-page-templates/
+
 Timber::render( $templates, $context );
