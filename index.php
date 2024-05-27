daa4225bd7e31d4eb4a3316d072ad4f12f2f4a2e
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-05-27 22:56:35 +0300

    Tune tiedotteet section and calendar

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $context          = Timber::context();
 $context['news'] = Timber::get_posts([
 	'post_type'	=> 'post',
 	'category_name' => 'tiedote',
-	'posts_per_page' => 20,
+	'posts_per_page' => 5,
 ]);
 
 $context['posts'] = Timber::get_posts([
