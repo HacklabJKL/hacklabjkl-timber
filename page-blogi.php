fd44eca3e4028f4cfaa56df734fd9905243d1ec8
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-05-27 22:41:21 +0300

    Show all posts in blogi page for now when pagination is broken

--- page-blogi.php
+++ page-blogi.php
@@ -3,7 +3,7 @@ $context = Timber::context();
 
 $context['posts'] = Timber::get_posts([
 	'post_type'	=> 'post',
-	'posts_per_page' => 20,
+	'posts_per_page' => 200,
 ]);
 
 Timber::render( 'page-blogi.twig', $context );
